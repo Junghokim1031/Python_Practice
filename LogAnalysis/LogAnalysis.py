f98083e5069ac9e5a1b597143b69993f96dee2e5
Jung Ho Kim <junghokim1031@gmail.com>
2026-03-03 10:26:49 +0900

    Project: Log Analysis Feature: Matplotlib component
    Comment: README는 코드를 Gemini를 통하여 생성하였습니다.

--- LogAnalysis/LogAnalysis.py
+++ LogAnalysis/LogAnalysis.py
@@ -1,25 +1,9 @@
-from sys import argv
 import datetime
 import time
 import random
 import pandas as pd
 
 servers = ["Server A", "Server B", "Server C"]
-#actions = ["Log in", "Log Out", "Action A", "Action B", "Action C"]
-#errrors = ["Error A", "Error B", "Error C"]
-#warnings = ["Warning A", "Warning B", "Warning C"]
-
-
-
-#with open("Logfile.txt", "a") as file:
-#    for x in range(1000):
-#        server = random.choice(servers)
-#        action = random.choice(actions)
-#        error = random.choice(errrors)
-#        warning = random.choice(warnings)
-#        timestamp = datetime.datetime.now()
-#        file.write("%s: %s: %s: %s: %s\n" %(server, action, error, warning,timestamp))
-#    print("End of Log")
 
 with open("Server_Log.csv", "w") as file:
     file.write("Timestamp, Server, CPU Usage, Memory Usage, Disk Usage\n")
